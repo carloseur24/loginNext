bb4a91c28b32dd19af763e3eba78e6353d41627e
klimker <carloseur24@gmail.com>
2022-09-16 17:37:25 -0400

    Tailwind Changes

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,13 @@
-// import { SessionProvider } from "next-auth/react"
-import "../styles/global.css";
+import { SessionProvider } from "next-auth/react"
+import "../styles/global.css"
 
-
-// function MyApp({ Component, pageProps }) {
-// return <Component {...pageProps} />;
-// }
-
-// export default function App({
-//   Component,
-//   pageProps: { session, ...pageProps },
-// }) {
-//   return (
-//     <SessionProvider session={session}>
-//       <Component {...pageProps} />
-//     </SessionProvider>
-//   )
-// }
-
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <Component {...pageProps} />
+    </SessionProvider>
+  )
+}
--- pages/p/[id].jsx
+++ pages/p/[id].jsx
@@ -4,13 +4,6 @@ import { makeSerializable } from "../../lib/util"
 import prisma from "../../lib/prisma"
 import { MdDeleteForever } from "react-icons/md"
 
-// async function publish(id) {
-//   await fetch(`/api/publish/${id}`, {
-//     method: "PUT",
-//   })
-//   await Router.push("/")
-// }
-
 async function destroy(id) {
   await fetch(`/api/post/${id}`, {
     method: "DELETE",
@@ -19,47 +12,36 @@ async function destroy(id) {
 }
 
 const Post = (props) => {
-  // if (!props.published) {
-  //   title = `${title} (Draft)`
-  // }
   const remember = (
     <h1 style={{ marginTop: "0", marginBottom: "2rem" }}>
       Remember #{props.id}
     </h1>
   )
 
-  const date = props.createdAt
-  const split = date.split("-")
-  // console.log(props.feed.length)
   return (
-
     <Layout>
-
       <div className="page2">
-        <div class="text-indigo-600 font-bold text-center pb-10">{remember}</div>
+        <div className="text-indigo-600 font-bold text-center pb-10">
+          {remember}
+        </div>
       </div>
 
       <div className="flex page justify-between" key={props.id}>
-
         <div>
           <h2 className="title font-semibold text-lg">{props.title}</h2>
-          <small className="date">Date ( {props.createdAt } )</small>
+          <small className="date">Date ( {props.createdAt} )</small>
           <small className="tag"> | Tag: {props.content} </small>
         </div>
-        
+
         <div className="actions">
-          {/* {!props.published && (
-            <button onClick={() => publish(props.id)}>Publish</button>
-          )} */}
-          <button 
-          className= "mt-1 tracking-tight transition duration-500 ease  select-none hover:text-red-600  focus:outline-none focus:shadow-outline"
-          onClick={() => destroy(props.id)}>
-          <MdDeleteForever fontSize={"3rem"} />
+          <button
+            className="mt-1 tracking-tight transition duration-500 ease  select-none hover:text-red-600  focus:outline-none focus:shadow-outline"
+            onClick={() => destroy(props.id)}
+          >
+            <MdDeleteForever fontSize={"3rem"} />
           </button>
         </div>
-
       </div>
-
     </Layout>
   )
 }
